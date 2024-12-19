2ec1ef1297b2c8d58fac53fb9d76cd05d5be704e
Melanie Allmond <melanie.allmond@gmail.com>
2024-12-18 19:49:35 -0500

    added JWT in localStorage and incorporated JWT tokens into login process

--- frontend/src/users/LoginForm.jsx
+++ frontend/src/users/LoginForm.jsx
@@ -16,24 +16,28 @@ function LoginForm() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const response = await fetch(`http://localhost:5000/authentication/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
+    try {
+      const response = await fetch(`http://localhost:5000/authentication/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (response.status === 200) {
-      setCurrentUser(data.user);
-      history.push("/");
-    } else {
-      setErrorMessage(data.message);
+      if (response.ok) {
+        setCurrentUser(data.user);
+        localStorage.setItem("token", data.token);
+        history.push("/");
+      } else {
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      setErrorMessage("An error occurred. Please try again.");
     }
-
-    console.log(data);
   }
 
   return (
